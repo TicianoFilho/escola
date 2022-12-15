901f1d22002def1aa3007faeca56440e874cf546
TicianoFilho <ticianofilho@gmail.com>
2022-12-15 18:03:52 -0300

    create: AlunoBuilder class

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -11,7 +11,31 @@ public class Aluno {
 	private Email email;
 	private List<Telefone> telefones;
 	
+	public Aluno(CPF cpf, String nome, Email email) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+	}
+
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
+
+	public CPF getCpf() {
+		return cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	
+	
 }
---
+++ src/main/java/escola/AlunoBuilder.java
@@ -0,0 +1,20 @@
+package escola;
+
+public class AlunoBuilder {
+
+	private Aluno aluno;
+	
+	public AlunoBuilder comNomeCpfEmail(String nome, String cpf, String email) {
+		aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+		return this;
+	}
+	
+	public AlunoBuilder comTelefone(String ddd, String numero) {
+		aluno.adicionarTelefone(ddd, numero);
+		return this;
+	}
+	
+	public Aluno build() {
+		return this.aluno;
+	}
+}
